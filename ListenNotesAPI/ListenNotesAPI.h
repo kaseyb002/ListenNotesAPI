665d89adb22ebb54d6e154388ee2955f31eedcc3
Kasey Baughan <kbaughan@ancestry.com>
2019-10-22 11:48:19 -0600

    Initial Commit

---
+++ ListenNotesAPI/ListenNotesAPI.h
@@ -0,0 +1,19 @@
+//
+//  ListenNotesAPI.h
+//  ListenNotesAPI
+//
+//  Created by Kasey Baughan on 10/22/19.
+//  Copyright © 2019 Kasey Baughan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ListenNotesAPI.
+FOUNDATION_EXPORT double ListenNotesAPIVersionNumber;
+
+//! Project version string for ListenNotesAPI.
+FOUNDATION_EXPORT const unsigned char ListenNotesAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListenNotesAPI/PublicHeader.h>
+
+
