eb54b15a425c618a3c380c3027f16b935ec4dc10
Kasey Baughan <kbaughan@ancestry.com>
2020-05-05 11:34:58 -0600

    Fixed isOn criteria

--- ListenNotesAPI/Model/Extensions/LNSearchFilter+Extensions.swift
+++ ListenNotesAPI/Model/Extensions/LNSearchFilter+Extensions.swift
@@ -10,8 +10,19 @@ import Foundation
 
 public extension LNSearchFilter {
     
+    /// Whether the default settings have been changed
     var isOn: Bool {
-        guard let data = try? JSONEncoder().encode(self) else { return false }
-        return !data.isEmpty
+        if sortBy == .mostRecent { return true }
+        if searchInFields != [.everything] { return true }
+        if minMinuteLength != nil { return true }
+        if maxMinuteLength != nil { return true }
+        if publishedAfter != nil { return true }
+        if publishedBefore != nil { return true }
+        if safeMode != .off { return true }
+        if language != nil { return true }
+        if !genreIds.isEmpty { return true }
+        if podcastId != nil { return true }
+        
+        return false
     }
 }
